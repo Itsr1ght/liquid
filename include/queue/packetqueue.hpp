c2b058f581fb266f7934295b42c124054cacc0b1
Bilal <bjlegend007@gmail.com>
2024-12-25 22:51:35 +0530

    small updates

--- include/queue/packetqueue.hpp
+++ include/queue/packetqueue.hpp
@@ -4,29 +4,28 @@
 **  Includes
 */
 
-extern "C"{
-    #include "libavutil/avstring.h"
-    #include "libavutil/channel_layout.h"
-    #include "libavutil/eval.h"
-    #include "libavutil/mathematics.h"
-    #include "libavutil/pixdesc.h"
-    #include "libavutil/imgutils.h"
-    #include "libavutil/dict.h"
-    #include "libavutil/fifo.h"
-    #include "libavutil/parseutils.h"
-    #include "libavutil/samplefmt.h"
-    #include "libavutil/time.h"
-    #include "libavutil/bprint.h"
-    #include "libavformat/avformat.h"
-    #include "libavdevice/avdevice.h"
-    #include "libswscale/swscale.h"
-    #include "libavutil/opt.h"
-    #include "libavcodec/avfft.h"
-    #include "libavcodec/avcodec.h"
-    #include "libswresample/swresample.h"
-    #include "libavformat/avformat.h"
-    #include "libavfilter/buffersink.h"
-    #include "libavfilter/buffersrc.h"
+extern "C" {
+#include "libavcodec/avcodec.h"
+#include "libavcodec/avfft.h"
+#include "libavdevice/avdevice.h"
+#include "libavfilter/buffersink.h"
+#include "libavfilter/buffersrc.h"
+#include "libavformat/avformat.h"
+#include "libavutil/avstring.h"
+#include "libavutil/bprint.h"
+#include "libavutil/channel_layout.h"
+#include "libavutil/dict.h"
+#include "libavutil/eval.h"
+#include "libavutil/fifo.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/mathematics.h"
+#include "libavutil/opt.h"
+#include "libavutil/parseutils.h"
+#include "libavutil/pixdesc.h"
+#include "libavutil/samplefmt.h"
+#include "libavutil/time.h"
+#include "libswresample/swresample.h"
+#include "libswscale/swscale.h"
 }
 
 #include "SDL.h"
@@ -37,19 +36,19 @@ extern "C"{
 */
 
 struct LiquidAVPacketList {
-    AVPacket *pkt;
-    int serial;
+  AVPacket *pkt;
+  int serial;
 };
 
-struct PacketQueue{
-    AVFifoBuffer *pkt_list;
-    int nb_packets;
-    int size;
-    int64_t duration;
-    int abort_request;
-    int serial;
-    SDL_mutex *mutex;
-    SDL_cond *cond;
+struct PacketQueue {
+  AVFifoBuffer *pkt_list;
+  int nb_packets;
+  int size;
+  int64_t duration;
+  int abort_request;
+  int serial;
+  SDL_mutex *mutex;
+  SDL_cond *cond;
 };
 
 /*
@@ -60,7 +59,8 @@ int packet_queue_init(PacketQueue *q);
 void packet_queue_start(PacketQueue *q);
 int packet_queue_put(PacketQueue *q, AVPacket *pkt);
 int packet_queue_put_private(PacketQueue *q, AVPacket *pkt);
-int packet_queue_put_nullpacket(PacketQueue *q, AVPacket *pkt, int stream_index);
+int packet_queue_put_nullpacket(PacketQueue *q, AVPacket *pkt,
+                                int stream_index);
 int packet_queue_get(PacketQueue *q, AVPacket *pkt, int block, int *serial);
 void packet_queue_abort(PacketQueue *q);
 void packet_queue_destroy(PacketQueue *q);
